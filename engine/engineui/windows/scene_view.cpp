9f0dd963355b7fb58304ab8e45a27fc2d5ae1934
Jon <blackstormdevelopment@gmail.com>
2024-11-08 16:21:23 +0100

    add stack layout

--- engine/engineui/windows/scene_view.cpp
+++ engine/engineui/windows/scene_view.cpp
@@ -4,15 +4,48 @@ void SceneView::prepare()
 {
 	ImGui::Begin("  Scene View  ", nullptr, EngineUI::windowFlags.standard);
 
-	UIComponents::headline("Scene View", ICON_FA_MAP);
+        float windowWidth = ImGui::GetWindowWidth();
+        float contentWidth = ImGui::GetContentRegionAvail().x;
+        float rightPadding = windowWidth - contentWidth;
 
-	UIComponents::toggleButton(Runtime::wireframe, ICON_FA_VECTOR_SQUARE, 0, "Wireframe");
-	ImGui::SameLine();
-	UIComponents::toggleButton(Runtime::solidMode, ICON_FA_CUBE, 1, "Solid Mode");
-	ImGui::SameLine();
-	UIComponents::toggleButton(Runtime::shadows, ICON_FA_ECLIPSE, 2, "Shadows");
-	ImGui::SameLine();
-	UIComponents::toggleButton(Runtime::postProcessingEffects, ICON_FA_SPARKLES, 3, "Post Processing");
+        ImVec2 layout_size = ImVec2(windowWidth - rightPadding, 45.0f);
+
+        static ImVec2 item_spacing = ImVec2(10.0f, 0.0f);
+        ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(floorf(item_spacing.x), floorf(item_spacing.y)));
+        ImGui::PushID(1);
+        ImGui::BeginHorizontal("h1", layout_size, 0.5f);
+            ImGui::Spring(0.5f);
+
+            float currentY = ImGui::GetCursorPosY();
+            float padding = 2.5f;
+            ImGui::SetCursorPosY(currentY + padding);
+            ImGui::Text(ICON_FA_MAP);
+            ImGui::SetCursorPosY(currentY);
+
+            ImGui::PushFont(EngineUI::fonts.uiHeadline);
+            ImGui::Text("Scene View");
+            ImGui::PopFont();
+
+            ImGui::Spring(0.5f);
+        ImGui::EndHorizontal();
+        ImGui::PopID();
+        ImGui::PopStyleVar();
+
+        ImGui::PushID(2);
+        ImGui::BeginHorizontal("h1", layout_size, 0.5f);
+            ImGui::Spring(0.5f);
+
+            UIComponents::toggleButton(Runtime::wireframe, ICON_FA_VECTOR_SQUARE, 0, "Wireframe");
+            UIComponents::toggleButton(Runtime::solidMode, ICON_FA_CUBE, 1, "Solid Mode");
+            UIComponents::toggleButton(Runtime::shadows, ICON_FA_ECLIPSE, 2, "Shadows");
+            UIComponents::toggleButton(Runtime::postProcessingEffects, ICON_FA_SPARKLES, 3, "Post Processing");
+
+            ImGui::Spring(0.5f);
+        ImGui::EndHorizontal();
+        ImGui::PopID();
+
+        ImDrawList* draw_list = ImGui::GetWindowDrawList();
+        draw_list->AddRect(ImGui::GetItemRectMin(), ImGui::GetItemRectMax(), ImGui::GetColorU32(ImGuiCol_Border));
 
 	ImGui::End();
 }
